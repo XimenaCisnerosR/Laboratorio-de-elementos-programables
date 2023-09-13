8a9d645b00be94ef9d9263c67455a57eef68acbd
XimenaCisnerosR <194460@iberopuebla.mx>
2023-09-13 10:32:50 -0600

    .

--- practica3/files/4AttinyDigitalDELAY/4AttinyDigitalDELAY.ino
+++ practica3/files/4AttinyDigitalDELAY/4AttinyDigitalDELAY.ino
@@ -9,11 +9,11 @@ void setup()
 void loop()
 {
   digitalWrite(3, HIGH);
-  delay(300);
+  delay(500);
   digitalWrite(3, LOW);
-  delay(300);
+  delay(500);
   digitalWrite(4, HIGH);
-  delay(300);
+  delay(500);
   digitalWrite(4, LOW);
-  delay(300);
+  delay(500);
 }
